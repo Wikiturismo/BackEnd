900534708b67ecb6d0889bd6d7937223f9a7844a
Diego Guevara <diaguevararo@unal.edu.co>
2017-03-24 20:13:06 -0500

    Associations user and town

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,3 +1,6 @@
 class Town < ApplicationRecord
   belongs_to :depart
+  has_many :places, dependent: :destroy
+  has_many :images, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
