9ce5758016eda1e22f044a6b85bd2cf46976b553
Juan Corredor <jumcorredorro@unal.edu.co>
2017-03-24 20:19:26 -0500

    Validations depart, place and town.

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,4 +5,9 @@ class Place < ApplicationRecord
   has_many :images, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :schedules, dependent: :destroy
+
+  validates :name,:state,:publicationdate,:description, :ubication, :address, :type, :depart_id, :town_id, :user_id, presence: true
+  validates :name,:ubication,:address, :type, length: {minimum: 3, message: "Debe ser mayor a 3 caracteres"}
+  validates :valoration, numericality: {only_integer: true, :greater_than => 0, :less_than => 50 ,message: "Debe estar entre 0 y 50"}
+  validates :entrycost, numericality: {only_integer: true, :greater_than => 0,message: "Debe ser mayor a 0"}
 end
