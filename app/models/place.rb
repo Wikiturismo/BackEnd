0ceec4bb5359defbffd3f405fd0530957e1be9ff
Kevin Mendez <kmendezp@unal.edu.co>
2017-03-24 20:07:04 -0500

    Validations on depart and place

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,7 @@ class Place < ApplicationRecord
   belongs_to :town
   belongs_to :depart
   belongs_to :user
+  has_many :images, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :schedules, dependent: :destroy
 end
