8aa8a8f1c87ff48e4ebd3277532b21c95c39374b
Diego Guevara <diaguevararo@unal.edu.co>
2017-03-28 21:02:13 -0500

    Places queries

--- app/models/place.rb
+++ app/models/place.rb
@@ -10,4 +10,30 @@ class Place < ApplicationRecord
   validates :name,:ubication,:address, :type, length: {minimum: 3, message: "Debe ser mayor a 3 caracteres"}
   validates :valoration, numericality: {only_integer: true, :greater_than => 0, :less_than => 50 ,message: "Debe estar entre 0 y 50"}
   validates :entrycost, numericality: {only_integer: true, :greater_than => 0,message: "Debe ser mayor a 0"}
+
+  def self.load_places(page = 1, per_page = 10)
+    includes(:images,:towns,:comments:users:schedules)
+    .paginate(:page => page, :per_page => per_page)
+  end
+
+  def self.places_by_id(id)
+    includes(:images,:towns,:comments:users:schedules)
+    .find_by_id(id)
+  end
+
+  def self.places_by_name(name,page = 1, per_page = 10)
+    load_dplaces(page,per_page)
+    .where("places.name LIKE ?", "#{name.downcase}")
+  end
+
+  def self.places_by_schedule(schedule,page = 1, per_page = 10)
+    load_dplaces(page,per_page)
+    .where("places.name LIKE ?", "#{name.downcase}")
+  end
+
+
+
+
+
+
 end
