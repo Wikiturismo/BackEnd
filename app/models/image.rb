74417842a8e2fc08b86bf376c98fe032bfbb02e1
Kevin Mendez <kmendezp@unal.edu.co>
2017-03-30 22:54:41 -0500

    fix querys images

--- app/models/image.rb
+++ app/models/image.rb
@@ -10,26 +10,31 @@ class Image < ApplicationRecord
   validates :width, numericality: {only_integer: true, :greater_than => 99,message: "Debe ser mayor a 100"}
 
   def self.load_images(page = 1, per_page = 10)
-        includes(town:[:comments,:places],place:[:comments,:schedules],depart:[:images,:towns])
+        includes(town:[:comments,:places],place:[:comments,:schedules],depart:[:towns])
         .paginate(:page => page, :per_page => per_page)
     end
   def self.images_by_id(id)
-    includes(town:[:comments,:places],place:[:comments,:schedules],depart:[:images,:towns])
+    includes(town:[:comments,:places],place:[:comments,:schedules],depart:[:towns])
     .find_by_id(id)
   end
 
 
-  def self.images_by_depart(depart,page = 1, per_page = 10)
-        load_images(page,per_page)
-        .where("images.depart_id LIKE ?", "#{depart.downcase}")
+  def self.images_by_depart(name,page = 1, per_page = 10)
+        joins(:departs)
+            .where("departs.name LIKE ? AND images.depart_id=depart.id", "#{name.downcase}")
+                .select("image. *")
+                    .paginate(:page => page,:per_page => per_page)
     end
-
-  def self.images_by_town(town,page = 1, per_page = 10)
-        load_images(page,per_page)
-        .where("images.towns LIKE ?", "#{town.downcase}")
+    def self.images_by_town(name,page = 1, per_page = 10)
+        joins(:towns)
+            .where("towns.name LIKE ? AND images.town_id=town.id", "#{name.downcase}")
+                .select("image. *")
+                    .paginate(:page => page,:per_page => per_page)
     end
-  def self.images_by_place(place,page = 1, per_page = 10)
-        load_images(page,per_page)
-        .where("place.departs LIKE ?", "#{depart.downcase}")
+    def self.images_by_place(name,page = 1, per_page = 10)
+        joins(:places)
+            .where("places.name LIKE ? AND images.place_id=place.id", "#{name.downcase}")
+                .select("image. *")
+                    .paginate(:page => page,:per_page => per_page)
     end
 end
