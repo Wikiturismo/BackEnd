988f7414159671e29cc3972e9b7b5a50e9c931d8
Diego Guevara <diaguevararo@unal.edu.co>
2017-04-19 01:02:15 -0500

    CarrierWave(local)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :imgtowns
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :imgtowns
       resources :departs , only: [:show, :index] do
         collection do
           get '/name/:name', to: 'departs#name', as: 'departname'
